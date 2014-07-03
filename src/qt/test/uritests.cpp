f9e30165b543631296d3d8779187be23acd07ce6
coinsolidation <coinsolidation@gmail.com>
2014-07-03 19:50:47 +0100

    bitmark: uri support and test update

--- src/qt/test/uritests.cpp
+++ src/qt/test/uritests.cpp
@@ -9,54 +9,54 @@ void URITests::uriTests()
 {
     SendCoinsRecipient rv;
     QUrl uri;
-    uri.setUrl(QString("bitmark:175tWpb8K1S7NmH4Zx6rewF9WQrcZv245W?req-dontexist="));
+    uri.setUrl(QString("bitmark:bGfikGxWjawwxnBQSsinRZXFFQNSoudzk8?req-dontexist="));
     QVERIFY(!GUIUtil::parseBitmarkURI(uri, &rv));
 
-    uri.setUrl(QString("bitmark:175tWpb8K1S7NmH4Zx6rewF9WQrcZv245W?dontexist="));
+    uri.setUrl(QString("bitmark:bGfikGxWjawwxnBQSsinRZXFFQNSoudzk8?dontexist="));
     QVERIFY(GUIUtil::parseBitmarkURI(uri, &rv));
-    QVERIFY(rv.address == QString("175tWpb8K1S7NmH4Zx6rewF9WQrcZv245W"));
+    QVERIFY(rv.address == QString("bGfikGxWjawwxnBQSsinRZXFFQNSoudzk8"));
     QVERIFY(rv.label == QString());
     QVERIFY(rv.amount == 0);
 
-    uri.setUrl(QString("bitmark:175tWpb8K1S7NmH4Zx6rewF9WQrcZv245W?label=Wikipedia Example Address"));
+    uri.setUrl(QString("bitmark:bGfikGxWjawwxnBQSsinRZXFFQNSoudzk8?label=Bitmark Example Address"));
     QVERIFY(GUIUtil::parseBitmarkURI(uri, &rv));
-    QVERIFY(rv.address == QString("175tWpb8K1S7NmH4Zx6rewF9WQrcZv245W"));
-    QVERIFY(rv.label == QString("Wikipedia Example Address"));
+    QVERIFY(rv.address == QString("bGfikGxWjawwxnBQSsinRZXFFQNSoudzk8"));
+    QVERIFY(rv.label == QString("Bitmark Example Address"));
     QVERIFY(rv.amount == 0);
 
-    uri.setUrl(QString("bitmark:175tWpb8K1S7NmH4Zx6rewF9WQrcZv245W?amount=0.001"));
+    uri.setUrl(QString("bitmark:bGfikGxWjawwxnBQSsinRZXFFQNSoudzk8?amount=0.001"));
     QVERIFY(GUIUtil::parseBitmarkURI(uri, &rv));
-    QVERIFY(rv.address == QString("175tWpb8K1S7NmH4Zx6rewF9WQrcZv245W"));
+    QVERIFY(rv.address == QString("bGfikGxWjawwxnBQSsinRZXFFQNSoudzk8"));
     QVERIFY(rv.label == QString());
     QVERIFY(rv.amount == 100000);
 
-    uri.setUrl(QString("bitmark:175tWpb8K1S7NmH4Zx6rewF9WQrcZv245W?amount=1.001"));
+    uri.setUrl(QString("bitmark:bGfikGxWjawwxnBQSsinRZXFFQNSoudzk8?amount=1.001"));
     QVERIFY(GUIUtil::parseBitmarkURI(uri, &rv));
-    QVERIFY(rv.address == QString("175tWpb8K1S7NmH4Zx6rewF9WQrcZv245W"));
+    QVERIFY(rv.address == QString("bGfikGxWjawwxnBQSsinRZXFFQNSoudzk8"));
     QVERIFY(rv.label == QString());
     QVERIFY(rv.amount == 100100000);
 
-    uri.setUrl(QString("bitmark:175tWpb8K1S7NmH4Zx6rewF9WQrcZv245W?amount=100&label=Wikipedia Example"));
+    uri.setUrl(QString("bitmark:bGfikGxWjawwxnBQSsinRZXFFQNSoudzk8?amount=100&label=Bitmark Example"));
     QVERIFY(GUIUtil::parseBitmarkURI(uri, &rv));
-    QVERIFY(rv.address == QString("175tWpb8K1S7NmH4Zx6rewF9WQrcZv245W"));
+    QVERIFY(rv.address == QString("bGfikGxWjawwxnBQSsinRZXFFQNSoudzk8"));
     QVERIFY(rv.amount == 10000000000LL);
-    QVERIFY(rv.label == QString("Wikipedia Example"));
+    QVERIFY(rv.label == QString("Bitmark Example"));
 
-    uri.setUrl(QString("bitmark:175tWpb8K1S7NmH4Zx6rewF9WQrcZv245W?message=Wikipedia Example Address"));
+    uri.setUrl(QString("bitmark:bGfikGxWjawwxnBQSsinRZXFFQNSoudzk8?message=Bitmark Example Address"));
     QVERIFY(GUIUtil::parseBitmarkURI(uri, &rv));
-    QVERIFY(rv.address == QString("175tWpb8K1S7NmH4Zx6rewF9WQrcZv245W"));
+    QVERIFY(rv.address == QString("bGfikGxWjawwxnBQSsinRZXFFQNSoudzk8"));
     QVERIFY(rv.label == QString());
 
-    QVERIFY(GUIUtil::parseBitmarkURI("bitmark://175tWpb8K1S7NmH4Zx6rewF9WQrcZv245W?message=Wikipedia Example Address", &rv));
-    QVERIFY(rv.address == QString("175tWpb8K1S7NmH4Zx6rewF9WQrcZv245W"));
+    QVERIFY(GUIUtil::parseBitmarkURI("bitmark://bGfikGxWjawwxnBQSsinRZXFFQNSoudzk8?message=Bitmark Example Address", &rv));
+    QVERIFY(rv.address == QString("bGfikGxWjawwxnBQSsinRZXFFQNSoudzk8"));
     QVERIFY(rv.label == QString());
 
-    uri.setUrl(QString("bitmark:175tWpb8K1S7NmH4Zx6rewF9WQrcZv245W?req-message=Wikipedia Example Address"));
+    uri.setUrl(QString("bitmark:bGfikGxWjawwxnBQSsinRZXFFQNSoudzk8?req-message=Bitmark Example Address"));
     QVERIFY(GUIUtil::parseBitmarkURI(uri, &rv));
 
-    uri.setUrl(QString("bitmark:175tWpb8K1S7NmH4Zx6rewF9WQrcZv245W?amount=1,000&label=Wikipedia Example"));
+    uri.setUrl(QString("bitmark:bGfikGxWjawwxnBQSsinRZXFFQNSoudzk8?amount=1,000&label=Bitmark Example"));
     QVERIFY(!GUIUtil::parseBitmarkURI(uri, &rv));
 
-    uri.setUrl(QString("bitmark:175tWpb8K1S7NmH4Zx6rewF9WQrcZv245W?amount=1,000.0&label=Wikipedia Example"));
+    uri.setUrl(QString("bitmark:bGfikGxWjawwxnBQSsinRZXFFQNSoudzk8?amount=1,000.0&label=Bitmark Example"));
     QVERIFY(!GUIUtil::parseBitmarkURI(uri, &rv));
 }

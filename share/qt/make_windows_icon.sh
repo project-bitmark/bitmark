b6df754e604b36edc2328208377dd4f1f285a719
coinsolidation <coinsolidation@gmail.com>
2014-07-06 19:53:42 +0100

    modify installer, toolbar images, clean icon sizes for windows

--- share/qt/make_windows_icon.sh
+++ share/qt/make_windows_icon.sh
@@ -9,23 +9,21 @@ ICON_DST_TEST=../../src/qt/res/icons/bitmark_testnet.ico
 convert ${ICON_SRC} -resize 16x16 ../pixmaps/bitmark16.png
 convert ${ICON_SRC} -resize 32x32 ../pixmaps/bitmark32.png
 convert ${ICON_SRC} -resize 48x48 bitmark-48.png
-convert ${ICON_SRC} -resize 64x64 ../pixmaps/bitmark64.png
-convert ${ICON_SRC} -resize 128x128 ../pixmaps/bitmark128.png
-convert ${ICON_SRC} -resize 256x256 ../pixmaps/bitmark256.png
+cp ${ICON_SRC} ../pixmaps/bitmark256.png
 # create mainnet windows icon with all variant sizes
-convert ../pixmaps/bitmark16.png ../pixmaps/bitmark32.png bitmark-48.png ../pixmaps/bitmark64.png ../pixmaps/bitmark128.png ../pixmaps/bitmark256.png ${ICON_DST}
+convert ../pixmaps/bitmark16.png ../pixmaps/bitmark32.png bitmark-48.png ../pixmaps/bitmark256.png ${ICON_DST}
+cp ../pixmaps/bitmark16.png ../../src/qt/res/icons/toolbar.png
 # create base for all testnet icons 
 convert ${ICON_SRC_TEST} -resize 16x16 bitmark-test-16.png
 convert ${ICON_SRC_TEST} -resize 32x32 bitmark-test-32.png
 convert ${ICON_SRC_TEST} -resize 48x48 bitmark-test-48.png
-convert ${ICON_SRC_TEST} -resize 64x64 bitmark-test-64.png
-convert ${ICON_SRC_TEST} -resize 128x128 bitmark-test-128.png
-convert ${ICON_SRC_TEST} -resize 256x256 bitmark-test-256.png
+cp ${ICON_SRC_TEST} bitmark-test-256.png
 # create testnet windows icon with all varient sizes
-convert bitmark-test-16.png bitmark-test-32.png bitmark-test-48.png bitmark-test-64.png bitmark-test-128.png bitmark-test-256.png ${ICON_DST_TEST}
+convert bitmark-test-16.png bitmark-test-32.png bitmark-test-48.png bitmark-test-256.png ${ICON_DST_TEST}
+cp bitmark-test-16.png ../../src/qt/res/icons/toolbar_testnet.png
 # create pixmap ico and xpm files
 convert ../pixmaps/bitmark16.png ../pixmaps/favicon.ico
-convert ../pixmaps/bitmark64.png ../pixmaps/bitmark.ico
+cp ${ICON_DST} ../pixmaps/bitmark.ico
 convert ../pixmaps/bitmark16.png ../pixmaps/bitmark16.xpm
 convert ../pixmaps/bitmark32.png ../pixmaps/bitmark32.xpm
 convert ../pixmaps/bitmark64.png ../pixmaps/bitmark64.xpm

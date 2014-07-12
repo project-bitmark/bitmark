ff8ca1264eb9bf1d2c7a1c89c164581c9939fcc3
coinsolidation <coinsolidation@gmail.com>
2014-07-12 20:09:48 +0100

    new icon

--- share/qt/make_icons.sh
+++ share/qt/make_icons.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # create multiresolution windows icon, and pixmaps
 # variables
-ICON_SRC=../../src/qt/res/icons/bitmark.png
+ICON_SRC=../pixmaps/bitmark1024.png
 ICON_DST=../../src/qt/res/icons/bitmark.ico
 ICON_SRC_TEST=../../src/qt/res/icons/bitmark_testnet.png
 ICON_DST_TEST=../../src/qt/res/icons/bitmark_testnet.ico
@@ -9,9 +9,13 @@ ICON_DST_TEST=../../src/qt/res/icons/bitmark_testnet.ico
 convert ${ICON_SRC} -resize 16x16 ../pixmaps/bitmark16.png
 convert ${ICON_SRC} -resize 32x32 ../pixmaps/bitmark32.png
 convert ${ICON_SRC} -resize 48x48 bitmark-48.png
-cp ${ICON_SRC} ../pixmaps/bitmark256.png
+convert ${ICON_SRC} -resize 64x64 ../pixmaps/bitmark64.png
+convert ${ICON_SRC} -resize 128x128 ../pixmaps/bitmark128.png
+convert ${ICON_SRC} -resize 256x256 ../pixmaps/bitmark256.png
+convert ${ICON_SRC} -resize 512x512 ../pixmaps/bitmark512.png
+cp ../pixmaps/bitmark256.png ../../src/qt/res/icons/bitmark.png
 # create mainnet windows icon with all variant sizes
-convert ../pixmaps/bitmark16.png ../pixmaps/bitmark32.png bitmark-48.png ../pixmaps/bitmark256.png ${ICON_DST}
+convert +matte ../pixmaps/bitmark16.png ../pixmaps/bitmark32.png bitmark-48.png ../pixmaps/bitmark256.png ${ICON_DST}
 cp ../pixmaps/bitmark16.png ../../src/qt/res/icons/toolbar.png
 # create base for all testnet icons 
 convert ${ICON_SRC_TEST} -resize 16x16 bitmark-test-16.png
@@ -30,7 +34,9 @@ convert ../pixmaps/bitmark64.png ../pixmaps/bitmark64.xpm
 convert ../pixmaps/bitmark128.png ../pixmaps/bitmark128.xpm
 convert ../pixmaps/bitmark256.png ../pixmaps/bitmark256.xpm
 # create apple icns file
+mv ../pixmaps/bitmark64.png ../pixmaps/tempbitmark64.png
 png2icns ../../src/qt/res/icons/bitmark.icns ../pixmaps/bitmark*.png
+mv ../pixmaps/tempbitmark64.png ../pixmaps/bitmark64.png
 # clean up
 rm bitmark-48.png
 rm bitmark-test-*.png

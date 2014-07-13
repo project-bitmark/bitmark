0dd9d0243cee4bbc012040d85b47a383323599b2
coinsolidation <coinsolidation@gmail.com>
2014-07-13 01:54:08 +0100

    remove genesis miner

--- src/main.cpp
+++ src/main.cpp
@@ -2950,67 +2950,6 @@ bool LoadBlockIndex()
     return true;
 }
 
-void static BitmarkGenesisMiner(CBlock block, int start, int threads)
-{
-    LogPrintf("BitmarkMiner started\n");
-    SetThreadPriority(THREAD_PRIORITY_LOWEST);
-    RenameThread("bitmark-miner");
-    block.nTime += start;
-    try { while (true) {
-		printf("Searching for genesis block...\n");
-		uint256 hashTarget = Params().ProofOfWorkLimit().getuint256();
-		uint256 thash;
-		char scratchpad[SCRYPT_SCRATCHPAD_SIZE];
-
-		while(true)
-		{
-			scrypt_1024_1_1_256_sp(BEGIN(block.nVersion), BEGIN(thash), scratchpad);
-			if (thash <= hashTarget)
-				break;
-			if ((block.nNonce & 0xFFF) == 0)
-			{
-				printf("nonce %08X: hash = %s (target = %s)\n", block.nNonce, thash.ToString().c_str(), hashTarget.ToString().c_str());
-			}
-			++block.nNonce;
-			if (block.nNonce == 0)
-			{
-				printf("NONCE WRAPPED, incrementing time\n");
-				block.nTime+=threads;
-			}
-		}
-		printf("block.nTime = %u \n", block.nTime);
-		printf("block.nNonce = %u \n", block.nNonce);
-		printf("block.GetHash = %s\n", block.GetHash().ToString().c_str());
-    } }
-    catch (boost::thread_interrupted)
-    {
-        LogPrintf("BitmarkMiner terminated\n");
-        throw;
-    }
-}
-
-void GenesisBitmark(CBlock block)
-{
-    static boost::thread_group* minerThreads = NULL;
-
-    int nThreads = boost::thread::hardware_concurrency();
-
-    if (minerThreads != NULL)
-    {
-        minerThreads->interrupt_all();
-        delete minerThreads;
-        minerThreads = NULL;
-    }
-
-    if (nThreads == 0)
-        return;
-
-    minerThreads = new boost::thread_group();
-    for (int i = 0; i < nThreads; i++)
-        minerThreads->create_thread(boost::bind(&BitmarkGenesisMiner, block, i, nThreads));
-}
-
-
 bool InitBlockIndex() {
     LOCK(cs_main);
     // Check whether we're already initialized
@@ -3024,24 +2963,6 @@ bool InitBlockIndex() {
 
     // Only add the genesis block if not reindexing (in which case we reuse the one already on disk)
     if (!fReindex) {
-    	CBlock &block = const_cast<CBlock&>(Params().GenesisBlock());
-
-        //// debug print
-    	uint256 hashGenesisBlock = uint256("0xf4dd31813f948437c0b8f0669f52ba62b5c237880716bf28d9b480bf81490659");
-        uint256 hash = block.GetHash();
-        printf("%s\n", hash.ToString().c_str());
-        printf("%s\n", hashGenesisBlock.ToString().c_str());
-        printf("%s\n", block.hashMerkleRoot.ToString().c_str());
-        assert(block.hashMerkleRoot == uint256("0xd4715adf41222fae3d4bf41af30c675bc27228233d0f3cfd4ae0ae1d3e760ba8"));
-
-        // If genesis block hash does not match, then generate new genesis hash.
-		if (true && block.GetHash() != hashGenesisBlock)
-		{
-			GenesisBitmark(block);
-		}
-
-        block.print();
-        while(true) {}
         try {
             CBlock &block = const_cast<CBlock&>(Params().GenesisBlock());
             // Start new block file

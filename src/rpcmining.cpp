47db8ed210bd4ec0851cfc10df694d7a7016d6ae
Andrew K <info@akrmn.com>
2017-08-02 16:34:09 +0000

    add miningalgo parameter to config

--- src/rpcmining.cpp
+++ src/rpcmining.cpp
@@ -53,7 +53,7 @@ void ShutdownRPCMining()
 #endif
 
 /* Set mining algo here for rpc mining */
-int miningAlgo = ALGO_SHA256D;
+int miningAlgo = GetArg("-miningalgo", ALGO_SHA256D);
 
 // Return average network hashes per second based on the last 'lookup' blocks,
 // or from the last difficulty change if 'lookup' is nonpositive.

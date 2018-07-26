f9edb92810c6fcea0a85cd5e4fae20af1e0df603
Andrew K <ak@seed.bitmark.co>
2018-07-26 20:36:57 +0000

    add version command to daemon

--- src/bitmarkd.cpp
+++ src/bitmarkd.cpp
@@ -103,6 +103,15 @@ bool AppInit(int argc, char* argv[])
             return false;
         }
 
+	if (mapArgs.count("-v") || mapArgs.count("--version"))
+	  {
+            // First part of help message is specific to bitmarkd / RPC client
+            std::string strUsage = _("Bitmark Core Daemon") + " " + _("version") + " " + FormatFullVersion() + "\n";
+	    fprintf(stdout, "%s", strUsage.c_str());
+	    return false;
+	  }
+	      
+
         // Command-line RPC
         bool fCommandLine = false;
         for (int i = 1; i < argc; i++)

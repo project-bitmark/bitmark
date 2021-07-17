539c4267648f35600b2b5d7e704a349639e915a3
Anton <anton@picapica>
2021-07-17 23:27:10 +0400

    Update RaiseFileDescriptorLimit() with latest implementation from Bitcoin source.

--- src/util.cpp
+++ src/util.cpp
@@ -1135,31 +1135,16 @@ int RaiseFileDescriptorLimit(int nMinFD) {
 #else
     struct rlimit limitFD;
     if (getrlimit(RLIMIT_NOFILE, &limitFD) != -1) {
-        // first request at least the amount passed in
         if (limitFD.rlim_cur < (rlim_t)nMinFD) {
             limitFD.rlim_cur = nMinFD;
             if (limitFD.rlim_cur > limitFD.rlim_max)
                 limitFD.rlim_cur = limitFD.rlim_max;
             setrlimit(RLIMIT_NOFILE, &limitFD);
+            getrlimit(RLIMIT_NOFILE, &limitFD);
         }
-        // then try to raise to the max we can
-        if (limitFD.rlim_cur < limitFD.rlim_max) {
-            limitFD.rlim_cur = limitFD.rlim_max;
-            setrlimit(RLIMIT_NOFILE, &limitFD);
-        }
-
-        if (getrlimit(RLIMIT_NOFILE, &limitFD) != -1) {
-            if (limitFD.rlim_cur < (rlim_t)nMinFD) {
-                LogPrintf("RaiseFileDescriptorLimit: could not raise limit to %lu fds, only %lu\n",
-                          (unsigned long)nMinFD,
-                          (unsigned long)limitFD.rlim_cur);
-            }
-            return limitFD.rlim_cur;
-        }
+        return limitFD.rlim_cur;
     }
-    LogPrintf("RaiseFileDescriptorLimit error: getrlimit(RLIMIT_NOFILE) returned %s\n", strerror(errno));
-
-    return nMinFD; // getrlimit failed, try to proceed
+    return nMinFD; // getrlimit failed, assume it's fine
 #endif
 }
 

554443f463bb7c0bce01b5793fe12009ff7808e7
Andrew Karam Aoun <ak@jikan.openmarks.com>
2023-12-08 10:51:49 +0000

    BN_zero openssl >=1.1 compatibility

--- src/key.cpp
+++ src/key.cpp
@@ -113,7 +113,11 @@ int ECDSA_SIG_recover_key_GFp(EC_KEY *eckey, ECDSA_SIG *ecsig, const unsigned ch
     if (!BN_bin2bn(msg, msglen, e)) { ret=-1; goto err; }
     if (8*msglen > n) BN_rshift(e, e, 8-(n & 7));
     zero = BN_CTX_get(ctx);
+    #if OPENSSL_VERSION_NUMBER < 0x11000000L
     if (!BN_zero(zero)) { ret=-1; goto err; }
+    #else
+    BN_zero(zero);
+    #endif
     if (!BN_mod_sub(e, zero, e, order, ctx)) { ret=-1; goto err; }
     rr = BN_CTX_get(ctx);
     if (!BN_mod_inverse(rr, r, order, ctx)) { ret=-1; goto err; }

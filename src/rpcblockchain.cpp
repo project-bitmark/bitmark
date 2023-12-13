3509b7d65aa64764cefd66e805627498a3016e56
Andrew Karam Aoun <ak@radiant.bitmark.co>
2023-12-13 16:38:48 +0000

    use MedianTimePast for GetAverageBlockSpacing

--- src/rpcblockchain.cpp
+++ src/rpcblockchain.cpp
@@ -321,10 +321,11 @@ double GetAverageBlockSpacing (const CBlockIndex * blockindex, const int algo, c
     }
     CountBlocks++;
     if(LastBlockTime > 0){
-      int64_t Diff = (LastBlockTime - BlockReading->GetBlockTime());
-      nActualTimespan += Diff;
+      nActualTimespan = LastBlockTime - BlockReading->GetMedianTimePast();
     }
-    LastBlockTime = BlockReading->GetBlockTime();
+    else {
+      LastBlockTime = BlockReading->GetMedianTimePast();
+    }
 
     BlockReading = BlockReading->pprev;
     
